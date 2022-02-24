67d74ce5925ba1f0b2ff21cd83d91299f3ade401
Adam Nelson <adam.nelson@teradata.com>
2022-02-24 14:34:22 -0500

    feat: add more examples to dialog overview

--- src/stories/dialog.stories.js
+++ src/stories/dialog.stories.js
@@ -28,3 +28,54 @@ Anatomy.parameters = {
     inlineStories: false,
   },
 };
+
+const templateLogout = ({}) => {
+
+  return `<mwc-dialog id="example-logout" heading="You will be logged out soon" scrimClickAction="" open>
+            Due to inactivity, you’ll be logged out in 3 minutes to protect your security.
+            <mwc-button slot="primaryAction">Log out</mwc-button>
+            <mwc-button slot="secondaryAction">Stay logged in</mwc-button>
+          </mwc-dialog>`;
+};
+
+export const Logout = templateLogout.bind({});
+Logout.parameters = {
+  docs: {
+    inlineStories: false,
+  },
+};
+
+const templateUDF = ({}) => {
+
+  return `<style>
+              mwc-formfield {
+                  margin-left: -.75rem;
+              }
+              .key-value {
+                margin-bottom: .5rem;
+                margin-top: .5rem;
+              }
+              .key-value--key {
+                color: --mdc-theme-text-secondary-on-background;
+              }
+          </style>
+          <mwc-dialog id="example-udf" heading="Delete User Defined Function" scrimClickAction="" open>
+            
+            <div class="key-value">
+              <div class="key-value--key mdc-typography--caption">Function to delete</div>
+              <div class="key-value--value">UDF_StrCpy</div>
+            </div>
+            <mwc-formfield label="I understand that all queries currently using this UDF will be canceled">
+              <mwc-checkbox class="child"></mwc-checkbox>
+            </mwc-formfield>
+            <mwc-button slot="primaryAction" class="destructive primary" disabled>Delete</mwc-button>
+            <mwc-button slot="secondaryAction">Cancel</mwc-button>
+          </mwc-dialog>`;
+};
+
+export const UDF = templateUDF.bind({});
+UDF.parameters = {
+  docs: {
+    inlineStories: false,
+  },
+};
