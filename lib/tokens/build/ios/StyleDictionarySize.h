9c0d529da851981bf8bfa2d975ce7d68751f281b
oseer <oseer.williams@gmail.com>
2022-05-05 21:49:03 -0400

    init styled dictionary tokens

---
+++ lib/tokens/build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Fri, 06 May 2022 01:38:49 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
