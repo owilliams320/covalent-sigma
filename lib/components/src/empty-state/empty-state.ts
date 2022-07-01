57faf3c25a0678d90bfdb4b980a48bc27fc7e4de
ecni19 <ericni2000@gmail.com>
2022-07-01 11:06:14 -0700

    cc-1/empty-state : changed button container to use slots

--- lib/components/src/empty-state/empty-state.ts
+++ lib/components/src/empty-state/empty-state.ts
@@ -15,25 +15,21 @@ export class CovalentEmptyStateBase extends LitElement {
   
   @property({type: String}) headline = '';
   @property({type: String}) subtitle = '';
-  @property({type: Boolean}) showSubtitle = false;
-  @property({type: Boolean}) action = false; 
-  @property({type: Boolean}) twoActions = false;
+
   override render() {
     return html`
       <div class="content mdc-typography">
         <div class="icon-background">
-          <td-icon class="covalent-icon">teradata</td-icon>
+          <td-icon class="covalent-icon">work</td-icon>
         </div>
         ${this.headline ? html`<h6 class="mdc-typography--headline6">${this.headline}</h6>` : html``}
-        ${this.subtitle ? html`<p key={line} class="mdc-typography--caption">${this.subtitle.split(/\n/).map(line => {
-          return html`${line}<br>`
-        })}</p>` : html``}
-        ${this.action || this.twoActions ? 
-        html`<div class="buttonContainer">
-          ${this.action || this.twoActions ? html`<td-button>Button Text</td-button>` : html``}
-          ${this.twoActions ? html`<td-button>Button Text</td-button>` : html``}
-        </div>` 
-        : html``}
+        ${this.subtitle ? html`
+        <p key={line} class="mdc-typography--caption">
+          ${this.subtitle.split(/\n/).map(line => { return html`${line}<br>`})}
+        </p>` : html``}
+        <div class="buttonContainer">
+          <slot></slot>
+        </div>
       </div>
     `;
   }
