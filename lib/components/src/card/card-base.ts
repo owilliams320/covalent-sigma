48de5ef46173bf4d9d1fb54f6e1efb48d32c8c63
ecni19 <ericni2000@gmail.com>
2022-07-06 12:14:13 -0700

    cc-1/empty-state : Combined card and page into one component

--- lib/components/src/card/card-base.ts
+++ lib/components/src/card/card-base.ts
@@ -1,9 +1,6 @@
-import { addHasRemoveClass, BaseElement } from '@material/mwc-base/base-element';
-import { CloseReason } from '@material/banner/constants';
-
-import { html, LitElement, TemplateResult } from 'lit';
+import { html, LitElement } from 'lit';
 import { classMap } from 'lit/directives/class-map';
-import { property, query } from 'lit/decorators';
+import { property } from 'lit/decorators';
 
 export class CardBase extends LitElement {
   /**
@@ -18,28 +15,58 @@ export class CardBase extends LitElement {
   @property()
   subTitle = '';
 
+  @property({type: Boolean}) actionBar = false;
   /**
    * Style the card as an outline variant
    */
   @property({type: Boolean}) outlined = false;
-   
+  @property({type: String}) emptyCaption = '';
   override render() {
     const classes = {
       'mdc-card': true,
       'mdc-card--outlined': this.outlined,
     };
     return html`
+      <style>
+        hr {
+          opacity: 0.12;
+          width: 100%;
+          margin: 0;
+        }
+        h6 {
+          margin: 16px;
+        }
+        .empty-state {
+          padding: 50px; 
+          width: 400px; 
+          height: 100px; 
+          display: flex; 
+          flex-direction: column; 
+          align-items: center; 
+          justify-content: center;
+        }  
+      </style>
       <div class="${classMap(classes)}">
+      ${this.cardTitle ? 
+        html`
+          <div class="mdc-card__actions mdc-typography--headline5">${this.cardTitle}</div>
+          <hr>
+        ` : ''}
         <div class="mdc-card__primary-action">
-            <!-- <div class="mdc-card__media mdc-card__media--square">
-                <div class="mdc-card__media-content">Title</div>
-            </div> -->
-            <slot></slot>
-            <div class="mdc-card__ripple"></div>
+          <slot name="card-content"></slot>
+          <slot name="empty-state"></slot>
+          ${this.emptyCaption ? 
+          html`<div class='empty-state'}>
+            <h6 class="mdc-typography--caption">${this.emptyCaption}</h6>
+            <slot name="emptyAction"></slot>
+          </div>` : ''}
         </div>
-        <div class="mdc-card__actions">
+        ${this.actionBar ? 
+        html`
+          <div class="mdc-card__actions">
             <slot name="card-actions"></slot>
-        </div>
+          </div>
+        ` : ''}
       </div>`;
   }
 }
