9f2e8bfa19819fd4803cce7f36aad387ac652d84
ecni19 <ericni2000@gmail.com>
2022-07-11 09:28:36 -0700

    Move css to external file

--- lib/components/src/toolbar/toolbar.ts
+++ lib/components/src/toolbar/toolbar.ts
@@ -1,43 +1,30 @@
 import { customElement, property } from "lit/decorators";
-import { styles } from '@material/mwc-top-app-bar/mwc-top-app-bar.css';
+import styles from './toolbar.scss';
 import { TopAppBarBase } from '@material/mwc-top-app-bar/mwc-top-app-bar-base';
 import { html } from "lit";
 
 declare global {
-    interface HTMLElementTagNameMap {
-        'td-toolbar': CovalentToolbarBase,
-    }
+  interface HTMLElementTagNameMap {
+    'td-toolbar': CovalentToolbarBase,
+  }
 }
 
 @customElement('td-toolbar')
 export class CovalentToolbarBase extends TopAppBarBase {
-    @property({type: Boolean}) breadcrumbs = false;
+  @property({type: Boolean}) breadcrumbs = false;
 
-    static override styles = [styles];
-    override render() {
-        return html
-        `
-            <style>
-                td-top-app-bar {
-                    --mdc-theme-primary: var(--mdc-theme-surface);
-                    --mdc-theme-on-primary: var(--mdc-theme-text-primary-on-background);
-                    border-bottom: solid 2px rgba(0,0,0,0.12);
-                }
-                [name="buttonActions"]::slotted(*) {
-                    margin: 8px;
-                }
-                [name="breadcrumbs"]::slotted(*) {
-                    padding-top: 20px;
-                }
-            </style>
-            <td-top-app-bar>
-                <slot name="title" slot="title"></slot>
-                <slot name="iconActions" slot="actionItems"></slot>
-                <slot name="buttonActions" slot="actionItems"></slot>
-            </td-top-app-bar class="breadcrumbs">
-            ${this.breadcrumbs ? html`<td-top-app-bar>
-                <div slot="title">Section title > Section title > Page title</div>
-            </td-top-app-bar>` : ''}
-        `
-    }
+  static override styles = [styles];
+  override render() {
+    return html
+    `
+    <td-top-app-bar>
+      <slot name="title" slot="title"></slot>
+      <slot name="iconActions" slot="actionItems"></slot>
+      <slot name="buttonActions" slot="actionItems"></slot>
+    </td-top-app-bar class="breadcrumbs">
+    ${this.breadcrumbs ? html`<td-top-app-bar>
+      <div slot="title">Section title > Section title > Page title</div>
+    </td-top-app-bar>` : ''}
+    `
+  }
 }
