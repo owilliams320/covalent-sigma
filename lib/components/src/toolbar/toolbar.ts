8d3ee25122f2289f7aff32cfef4aaa5d038d3014
ecni19 <ericni2000@gmail.com>
2022-07-13 12:31:37 -0700

    Add ... feature, remove top-app-bar from toolbar, add toolbar to app-shell

--- lib/components/src/toolbar/toolbar.ts
+++ lib/components/src/toolbar/toolbar.ts
@@ -16,25 +16,42 @@ export class CovalentToolbarBase extends TopAppBarBase {
   static override styles = [styles];
   override render() {
     // Create dummy data for the toolbar breadcrumbs.
-    const path: any[] = [];
-    for(let i = 0; i < this.breadcrumbs; i++) {
-      if(i == this.breadcrumbs-1) {
-        path.push(html`Page Title`);
-      }
-      else {
-        path.push(html`<span>Section title > </span>`);
+    let path: any[] = [];
+    if(this.breadcrumbs > 5) {
+      path.push(html`<span>Section title > ... > </span>`);
+      path.push('Page title');
+    }
+    else {
+      for(let i = 0; i < this.breadcrumbs; i++) {
+        if(i == this.breadcrumbs-1) {
+          path.push(`Page title`);
+        }
+        else {
+          path.push(html`<span>Section title > </span>`);
+        }
       }
     }
-
     return html`
-      <td-top-app-bar>
-        <slot name="title" slot="title"></slot>
-        <slot name="iconActions" slot="actionItems"></slot>
-        <slot name="buttonActions" slot="actionItems"></slot>
-      </td-top-app-bar>
-      ${this.breadcrumbs ? html`<td-top-app-bar>
-        <div slot="title" class="path">${path}</div>
-      </td-top-app-bar>` : ''}
+      <header class="mdc-top-app-bar">
+        <div class="mdc-top-app-bar__row">      
+          <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <slot name="title" slot="title"></slot>
+          </section>
+          <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+            <slot name="iconActions" slot="actionItems"></slot>
+            <slot name="buttonActions" slot="actionItems"></slot>
+          </section
+        </div>
+      </header>
+      ${this.breadcrumbs ? html`
+      <header class="mdc-top-app-bar">
+        <div class="mdc-top-app-bar__row">      
+          <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <div class="path">${path}</div>
+          </section>
+        </div>
+      </header>` 
+      : ''}
     `
   }
 }
