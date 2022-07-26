d757117c09b1921656787e4436b85965a93221e0
ecni19 <ericni2000@gmail.com>
2022-07-26 16:04:07 -0700

    Create page layout, changes to card to show more info

--- lib/components/src/card/card-base.ts
+++ lib/components/src/card/card-base.ts
@@ -22,27 +22,54 @@ export class CardBase extends LitElement {
    * Style the card as an outline variant
    */
   @property({type: Boolean}) outlined = false;
+  @property({type: Boolean}) divider = false;
+  @property({type: Boolean}) ripple = false;
+
+  @property({type: String}) sourceType = '';
+  @property({type: String}) sourceCreator = '';
   override render() {
     const classes = {
       'mdc-card': true,
       'mdc-card--outlined': this.outlined,
     };
+    const divider = {
+      'divider': this.divider,
+    };
+    const sourceType = {
+      'project': this.sourceType == 'project',
+      'dataset': this.sourceType == 'dataset',
+      'article': this.sourceType == 'article',
+      'file': this.sourceType == 'file',
+    }
     return html`
       <div class="${classMap(classes)}">
-      ${this.cardTitle ? 
-        html`
-          <div class="mdc-card__actions mdc-typography--headline5">${this.cardTitle}</div>
-        ` : ''}
-        <div class="mdc-card__primary-action">
-          <slot name="card-content"></slot>
-          <slot name="empty-state"></slot>
-        </div>
-        ${this.actionBar ? 
-        html`
-          <div class="mdc-card__actions">
-            <slot name="card-actions"></slot>
+      <div class="mdc-card__primary-action">
+          <div class="categoryContainer">
+            <div class="category ${classMap(sourceType)}">
+              <td-icon>
+                ${this.sourceType == 'project' ? 'folder' : 
+                  this.sourceType == 'dataset' ? 'database' :
+                  this.sourceType == 'file' ? 'description' :
+                  'bookmark'
+                }
+              </td-icon>
+              <div>${this.sourceType.charAt(0).toUpperCase() + this.sourceType.slice(1)}</div>
+            </div>
+            <div class="sourceCreator">${this.sourceCreator}</div>
           </div>
-        ` : ''}
+          ${this.cardTitle ? 
+            html`
+              <div class="mdc-card__actions mdc-typography--headline5 ${classMap(divider)}">${this.cardTitle}</div>
+            ` : ''}
+            <slot name="card-content" class="${this.ripple ? 'mdc-card__ripple' : ''}"></slot>
+            <slot name="empty-state"></slot>
+          </div>
+          ${this.actionBar ? 
+          html`
+            <div class="mdc-card__actions">
+              <slot name="card-actions"></slot>
+            </div>
+          ` : ''}
       </div>`;
   }
 }
