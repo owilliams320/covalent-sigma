c942c4b5b91e48fc7062a947615de5e9ff220753
ecni19 <ericni2000@gmail.com>
2022-08-05 12:23:20 -0700

    Clean up code and fix border bug

--- lib/components/src/icon-toggle/icon-check-toggle.ts
+++ lib/components/src/icon-toggle/icon-check-toggle.ts
@@ -1,6 +1,6 @@
 import { CheckboxBase } from '@material/mwc-checkbox/mwc-checkbox-base';
 import { styles as checkboxStyle } from '@material/mwc-checkbox/mwc-checkbox.css';
-import styles from './icon-check-toggle.scss';
+import styles from './icon-toggle.scss';
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
@@ -16,22 +16,21 @@ export class CovalentIconCheckToggleBase extends CheckboxBase {
     static override styles = [styles, checkboxStyle];
     @property({type:String}) width = "204";
     @property({type:String}) height = "162";
+    @property({type:Boolean}) iconOnly = false;
     override render() {
     const classes = {
         'checked': this.checked,
     };
     return html`
-        <div class="${classMap(classes)} container mdc-ripple-surface--primary" style="--width:${this.width == 'fill' ? `100%` : `${this.width}px`}; --height:${this.height}px;" @click="${this._handleClick}">
+        <div class="${classMap(classes)} container" style="--width:${this.width == 'fill' ? `100%` : `${this.width}px`}; --height:${this.height}px;" @click="${this._handleClick}">
             <input type="checkbox" class="mdc-checkbox__native-control"></input>
-            <div class="mdc-checkbox__background">
-                <svg class="mdc-checkbox__checkmark ${this.checked ? 'fill' : ''}" viewBox="0 0 24 24">
-                    <path class="mdc-checkbox__checkmark-path" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59"></path>
+            <div class="mdc-toggle__background">
+                <svg class="mdc-toggle__checkmark ${this.checked ? 'showCheck' : ''}" viewBox="0 0 24 24">
+                    <path class="mdc-toggle__checkmark-path ${this.checked ? 'showCheck' : ''}" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59"></path>
                 </svg>
             </div>
             <slot name="icon"></slot>
-            <div>
-                <slot name="text">First line</slot>
-            </div>
+            ${!this.iconOnly ? html`<div><slot name="text"></slot></div>` : ''}
         </div>
     `
   }
--- lib/components/src/icon-toggle/icon-radio-toggle.ts
+++ lib/components/src/icon-toggle/icon-radio-toggle.ts
@@ -1,6 +1,6 @@
 import { RadioBase } from '@material/mwc-radio/mwc-radio-base';
 import { styles as radioStyle } from '@material/mwc-radio/mwc-radio.css';
-import styles from './icon-radio-toggle.scss';
+import styles from './icon-toggle.scss';
 import { html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
@@ -13,28 +13,25 @@ declare global {
 
 @customElement('td-icon-radio-item')
 export class CovalentIconRadioToggleBase extends RadioBase {
-
-  static override styles = [radioStyle, styles];  
-
-  @property({type:String}) width = "204";
-  @property({type:String}) height = "162";
-  override render() {
-  const classes = {
-      'checked': this.checked,
-  };
-  return html`
-      <div @click="${() => {this.checked = true}}" class="${classMap(classes)} container mdc-ripple-surface--primary" style="--width:${this.width == 'fill' ? `100%` : `${this.width}px`}; --height:${this.height}px;">
-        <input type="radio" class="mdc-radio__native-control"></input>
-          <div class="mdc-radio__background">
-            <svg class="mdc-radio__checkmark ${this.checked ? 'showCheck' : ''}" viewBox="0 0 24 24">
-              <path class="mdc-radio__checkmark-path ${this.checked ? 'showCheck' : ''}" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59"></path>
-            </svg>
-          </div>
-          <slot name="icon"></slot>
-          <div>
-              <slot name="text">First line</slot>
-          </div>
-      </div>
-  `
-  }
+    static override styles = [styles, radioStyle];  
+    @property({type:String}) width = "204";
+    @property({type:String}) height = "162";
+    @property({type:Boolean}) iconOnly = false;
+    override render() {
+    const classes = {
+        'checked': this.checked,
+    };
+    return html`
+        <div @click="${() => {this.checked = true}}" class="${classMap(classes)} container" style="--width:${this.width == 'fill' ? `100%` : `${this.width}px`}; --height:${this.height}px;">
+            <input type="radio" class="mdc-radio__native-control"></input>
+            <div class="mdc-toggle__background">
+                <svg class="mdc-toggle__checkmark ${this.checked ? 'showCheck' : ''}" viewBox="0 0 24 24">
+                    <path class="mdc-toggle__checkmark-path ${this.checked ? 'showCheck' : ''}" fill="none" d="M1.73,12.91 8.1,19.28 22.79,4.59"></path>
+                </svg>
+            </div>
+            <slot name="icon"></slot>
+            ${!this.iconOnly ? html`<div><slot name="text"></slot></div>` : ''}
+        </div>
+    `
+    }
 }
